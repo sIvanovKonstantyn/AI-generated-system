09e9fe71c9382ef29abd06ea47793c34922ed835
ICKostiantyn.Ivanov <ICKostiantyn.Ivanov@amwell.com>
2023-12-11 21:01:12 +0100

    Cover persistence layer by tests

--- src/test/java/com/demos/paymentsprocessingservice/payments/repositories/PaymentHistoryITest.java
+++ src/test/java/com/demos/paymentsprocessingservice/payments/repositories/PaymentHistoryITest.java
@@ -1,31 +1,59 @@
 package com.demos.paymentsprocessingservice.payments.repositories;
 
 import com.demos.paymentsprocessingservice.payments.models.PaymentHistory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-//TODO add test containers
+@Testcontainers
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class PaymentHistoryITest {
+
+    @Container
+    static PostgreSQLContainer<?> dbContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres"))
+            .withUsername("postgres")
+            .withPassword("postgres");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", dbContainer::getJdbcUrl);
+    }
 
     @Autowired
     private PaymentHistoryRepository paymentHistoryRepository;
 
-    @Test
-    public void findAllByUserId() {
-        // Given
-        PaymentHistory paymentHistory = new PaymentHistory();
-        paymentHistory.setUserId("1234567890");
-        paymentHistoryRepository.save(paymentHistory);
+    @BeforeEach
+    void beforeEach() {
+        paymentHistoryRepository.deleteAll();
+    }
 
+    @AfterAll
+    static void shutDown() {
+        dbContainer.close();
+    }
+
+    @Test
+    public void findAllByUserIdShouldWorkAfterWeInsertedItemIntoDB() {
+        // Given
+        PaymentHistory paymentHistory = PaymentHistory.newPaymentHistory("1234567890", "description", LocalDateTime.now());
+        paymentHistoryRepository.insert(paymentHistory);
         // When
         List<PaymentHistory> paymentHistories = paymentHistoryRepository.findAllByUserId("1234567890");
-
         // Then
         assertThat(paymentHistories).hasSize(1);
         assertThat(paymentHistories.get(0).getUserId()).isEqualTo("1234567890");
